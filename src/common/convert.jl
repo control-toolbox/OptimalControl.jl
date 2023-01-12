03c18464de6eacfd15b70776a830b8643c0cfde0
Olivier Cots <olivier.cots@enseeiht.fr>
2023-01-12 22:34:10 +0100

    foo

---
+++ src/common/convert.jl
@@ -0,0 +1,15 @@
+#
+"""
+	convert(ocp::RegularOCPFinalCondition, ocp_type::DataType)
+
+TBW
+"""
+function convert(ocp::RegularOCPFinalCondition, ocp_type::DataType)
+    if ocp_type == RegularOCPFinalConstraint
+        c(x) = x - ocp.final_condition
+        ocp_new = OptimalControlProblem(ocp.Lagrange_cost, ocp.dynamics, ocp.initial_time, ocp.initial_condition, ocp.final_time, c, ocp.state_dimension, ocp.control_dimension, ocp.state_dimension, ocp.description...)
+    else
+        throw(IncorrectMethod(Symbol(ocp_type)))
+    end
+    return ocp_new
+end
