05391ee6545cb5eaa1f301e2149d050f5f69a13b
Olivier Cots <olivier.cots@enseeiht.fr>
2023-01-19 18:23:35 +0100

    replace simple shooting by shooting for direct

--- src/solve.jl
+++ src/solve.jl
@@ -5,23 +5,23 @@
 algorithmes = ()
 
 # descent methods
-algorithmes = add(algorithmes, (:direct, :simple_shooting, :descent, :bfgs, :bissection))
-algorithmes = add(algorithmes, (:direct, :simple_shooting, :descent, :bfgs, :backtracking))
-algorithmes = add(algorithmes, (:direct, :simple_shooting, :descent, :bfgs, :fixedstep))
-algorithmes = add(algorithmes, (:direct, :simple_shooting, :descent, :gradient, :bissection))
-algorithmes = add(algorithmes, (:direct, :simple_shooting, :descent, :gradient, :backtracking))
-algorithmes = add(algorithmes, (:direct, :simple_shooting, :descent, :gradient, :fixedstep))
+algorithmes = add(algorithmes, (:direct, :shooting, :descent, :bfgs, :bissection))
+algorithmes = add(algorithmes, (:direct, :shooting, :descent, :bfgs, :backtracking))
+algorithmes = add(algorithmes, (:direct, :shooting, :descent, :bfgs, :fixedstep))
+algorithmes = add(algorithmes, (:direct, :shooting, :descent, :gradient, :bissection))
+algorithmes = add(algorithmes, (:direct, :shooting, :descent, :gradient, :backtracking))
+algorithmes = add(algorithmes, (:direct, :shooting, :descent, :gradient, :fixedstep))
 
 function solve(prob::AbstractOptimalControlProblem, description...; kwargs...)
     method = getFullDescription(makeDescription(description...), algorithmes)
     # if no error before, then the method is correct: no need of else
     if :direct ∈ method
-        if :simple_shooting ∈ method
+        if :shooting ∈ method
             return solve_by_udss(prob, method; kwargs...)
         end
     end
 end
 
 function clean_description(d::Description)
-    return d\(:direct, :simple_shooting)
+    return d\(:direct, :shooting)
 end
