d2c20dbef25bf7efae47cf27bdf43791c8a0718e
gergaud <joseph.gergaud@toulouse-inp.fr>
2023-01-31 15:25:03 +0100

    foo

--- examples/basic.jl
+++ examples/basic.jl
@@ -14,6 +14,7 @@ B = [0.0; 1.0]
 f(x, u) = A * x + B * u[1];  # dynamics
 L(x, u) = 0.5 * u[1]^2   # integrand of the Lagrange cost
 
+
 # 
 # ocp = OptimalControlProblem(L, f, t0, x0, tf, xf, 2, 1)     # problem definition
 #  sol = solve(ocp)                                            # resolution
