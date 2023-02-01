a6aec88c84a0022fadf50cf054ee429addb86aee
gergaud <joseph.gergaud@toulouse-inp.fr>
2023-02-01 11:49:40 +0100

    foo

--- examples/basic.jl
+++ examples/basic.jl
@@ -23,8 +23,24 @@ objective!(ocp, :lagrangian, (x, u) -> 0.5*u^2) # default is to minimise
 
 # transcription ocp -> NLP
 N = 2 # time steps
-n = ocp.state_dimension
 m = ocp.control_dimension
+f = ocp.dynamics
+L = ocp.lagrange 
+
+# we pass u[1] because in our case ocp.dynamics and ocp.lagrange are define with a scalar u
+# but her x is a vector
+# question : how determine if u and x are scalar or vector ?
+# second member of the ode for the Mayer formulation
+n = ocp.state_dimension
+f_Mayer(x,u) = [f(x[1:n],u[1]); L(x[1:n],u[1])]
+# if isnothing(ocp.lagrange)
+#   n = ocp.state_dimension
+#   f_Mayer(x,u) = f(x,u[1])
+# else
+#   n = ocp.state_dimension + 1
+#   f_Mayer(x,u) = [f(x[1:n],u[1]); L(x[1:n],u[1])]
+# end
+
 nc = N*(n+1)+2*n+1               # dimension of the constraints
 # layout of the nlp unknown xu for Euler discretization 
 # additional state variable x_{n+1}(t) for the objective (Lagrange to Mayer formulation)
@@ -39,6 +55,38 @@ nc = N*(n+1)+2*n+1               # dimension of the constraints
 
                                #
 objective(xu) = xu[(N+1)*(n+1)]
+
+function get_state_at_time_step(xu,i)
+  """
+     return
+     x(t_i)
+  """
+  if i > N
+    error("trying to access at x(t_i) for i > N")
+  end  
+  return xu[1+i*(n+1):(i+1)*(n+1)]
+end
+
+function get_control_at_time_step(xu,i)
+  """
+     return
+     u(t_i)
+  """
+  if i > N-1
+    error("trying to access at (t_i) for i > N-1")
+  end
+  return xu[1+(N+1)*(n+1)+i*m:m+(N+1)*(n+1)+i*m]
+end
+
+function parse_sol(xu,t0,tf,N)
+  """
+     return
+      X = matrix(n+1,N+1)
+  """
+
+end
+
+
 
 #function constraint(ocp, xu::Vector{<:Real},N)::Vector{<:Real}
 function constraint(ocp, xu, N)
@@ -68,22 +116,16 @@ function constraint(ocp, xu, N)
     x0 = ocp.initial_condition
     xf = ocp.final_condition
   
-    f = ocp.dynamics
-   # f(x,u::Vector{<:Real}) = f(x,u[1])
-
-    L = ocp.lagrange 
-    
-    f_Mayer(x,u) = [f(x[1:n],u[1]); L(x[1:n],u[1])]   # second member of the ode for the Mayer formulation
+   
+                                                      
 
     h = (tf-t0)/N
     c = zeros(eltype(xu),nc)
     for i in 0:N-1
-      xi = xu[1+i*(n+1):(i+1)*(n+1)]
-      xip1 = xu[1+(i+1)*(n+1):(i+2)*(n+1)]
-      ui = xu[1+(N+1)*(n+1)+i*m:m+(N+1)*(n+1)+i*m]
+      xi = get_state_at_time_step(xu,i)
+      xip1 = get_state_at_time_step(xu,i+1)
+      ui = get_control_at_time_step(xu,i)
       c[1+i*(n+1):(i+1)*(n+1)] = xip1 - (xi + h*f_Mayer(xi, ui))
-      #c[1+i*(n+1):(i+1)*(n+1)] = xu[1+(i+1)*(n+1):(i+2)*(n+1)] - (xu[1+i*(n+1):(i+1)*(n+1)] + h*f_Mayer(xu[1+i*(n+1):(i+1)*(n+1)], xu[1+(N+1)*(n+1):m+(N+1)*(n+1)]))
-
     end
     c[1+N*(n+1):n+1+N*(n+1)] = xu[1:n+1] - [x0 ; 0]
     c[n+2+N*(n+1):2*n+1+N*(n+1)] = xu[1+N*(n+1):(N+1)*(n+1)-1] - xf
@@ -100,8 +142,10 @@ ub = zeros(nc)
 
 constraint_Ipopt(xu) = constraint(ocp,xu,N)
 xu0 = zeros((N+1)*(n+1)+N*m)  
+#println(get_state_at_time_step(xu0,1))
 
 #nlp = ADNLPModel(objective, xu0, constraint_Ipopt,lb,ub)
 nlp = ADNLPModel(objective, xu0, xu -> constraint(ocp,xu,N),lb,ub)
 stats = ipopt(nlp)
 print(stats)
+
