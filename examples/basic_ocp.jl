566b47bd7add286831a38549be98574e1e49c698
gergaud <joseph.gergaud@toulouse-inp.fr>
2023-02-01 17:50:57 +0100

    ajout basic_mayer et basic_ocp

---
+++ examples/basic_ocp.jl
@@ -0,0 +1,26 @@
+using OptimalControl, NLPModelsIpopt, ADNLPModels
+using Plots
+ocp = Model()
+#
+state!(ocp, 2)
+control!(ocp, 1)
+#
+time!(ocp, [0., 1.])
+#
+constraint!(ocp, :initial, [ -1., 0. ])
+constraint!(ocp, :final,   [  0., 0. ])
+#
+A = [ 0. 1.
+      0. 0.]
+B = [ 0.
+      1. ]
+constraint!(ocp, :dynamics, (x, u) -> A*x + B*u)
+#
+objective!(ocp, :lagrangian, (x, u) -> 0.5*u^2) # default is to minimise
+
+include("basic_mayer.jl")
+
+sol = solve(ocp,100)
+#println(sol)
+plot_sol(sol)
+
