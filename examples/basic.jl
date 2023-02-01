04ff12782d1e881b5af05ab494d08250257e8032
gergaud <joseph.gergaud@toulouse-inp.fr>
2023-02-01 09:38:33 +0100

    foo

--- examples/basic.jl
+++ examples/basic.jl
@@ -22,9 +22,10 @@ objective!(ocp, :lagrangian, (x, u) -> 0.5*u^2) # default is to minimise
 
 
 # transcription ocp -> NLP
-N = 10 # time steps
+N = 2 # time steps
 n = ocp.state_dimension
 m = ocp.control_dimension
+nc = N*(n+1)+2*n+1               # dimension of the constraints
 # layout of the nlp unknown xu for Euler discretization 
 # additional state variable x_{n+1}(t) for the objective (Lagrange to Mayer formulation)
 # [x_1(t_0), ... , x_{n+1}(t_0),
@@ -36,10 +37,11 @@ m = ocp.control_dimension
 
 
 
-xu0 = zeros((N+1)*(n+1)+N*m)                                 #
+                               #
 objective(xu) = xu[(N+1)*(n+1)]
 
-function constraint(ocp, xu::Vector{<:Real},N)::Vector{<:Real}
+#function constraint(ocp, xu::Vector{<:Real},N)::Vector{<:Real}
+function constraint(ocp, xu, N)
   """
   compute the constraints for the NLP : 
     - discretization of the dynamics via the Euler method
@@ -62,31 +64,44 @@ function constraint(ocp, xu::Vector{<:Real},N)::Vector{<:Real}
     tf = ocp.final_time
     n = ocp.state_dimension
     m = ocp.control_dimension
-  
+    nc = N*(n+1)+2*n+1               # dimension of the constraints
     x0 = ocp.initial_condition
     xf = ocp.final_condition
   
     f = ocp.dynamics
+   # f(x,u::Vector{<:Real}) = f(x,u[1])
+
     L = ocp.lagrange 
-    f_Mayer(x,u) = [f(x,u); L(x,u)]   # second member of the ode for the Mayer formulation
+    
+    f_Mayer(x,u) = [f(x[1:n],u[1]); L(x[1:n],u[1])]   # second member of the ode for the Mayer formulation
 
     h = (tf-t0)/N
-    c = zeros(N*(n+1)+2*n+1)
+    c = zeros(eltype(xu),nc)
     for i in 0:N-1
       xi = xu[1+i*(n+1):(i+1)*(n+1)]
       xip1 = xu[1+(i+1)*(n+1):(i+2)*(n+1)]
       ui = xu[1+(N+1)*(n+1):m+(N+1)*(n+1)]
       c[1+i*(n+1):(i+1)*(n+1)] = xip1 - (xi + h*f_Mayer(xi, ui))
+      c[1+i*(n+1):(i+1)*(n+1)] = xu[1+(i+1)*(n+1):(i+2)*(n+1)] - (xu[1+i*(n+1):(i+1)*(n+1)] + h*f_Mayer(xu[1+i*(n+1):(i+1)*(n+1)], xu[1+(N+1)*(n+1):m+(N+1)*(n+1)]))
+
     end
     c[1+N*(n+1):n+N*(n+1)] = xu[1:n] - x0
     c[n+1+N*(n+1):2*n+N*(n+1)] = xu[1+N*(n+1):(N+1)*(n+1)-1] - xf
+ 
     return c
 end
 
+
+
+
 # bounds for the constraints
-lb = zeros(N*(n+1)+2*n+1)
-ub = zeros(N*(n+1)+2*n+1)
+lb = zeros(nc)
+ub = zeros(nc)
+
 constraint_Ipopt(xu) = constraint(ocp,xu,N)
-nlp = ADNLPModel(objective, xu0, constraint_Ipopt,lb,ub)
+xu0 = zeros((N+1)*(n+1)+N*m)  
+
+#nlp = ADNLPModel(objective, xu0, constraint_Ipopt,lb,ub)
+nlp = ADNLPModel(objective, xu0, xu -> constraint(ocp,xu,N),lb,ub)
 stats = ipopt(nlp)
 print(stats)
