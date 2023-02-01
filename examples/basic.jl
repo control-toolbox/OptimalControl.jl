0732450946e2b2d791b376499022d11282792738
gergaud <joseph.gergaud@toulouse-inp.fr>
2023-02-01 14:52:45 +0100

    foo

--- examples/basic.jl
+++ examples/basic.jl
@@ -1,5 +1,5 @@
 using OptimalControl, NLPModelsIpopt, ADNLPModels
-
+using Plots
 ocp = Model()
 #
 state!(ocp, 2)
@@ -21,12 +21,54 @@ objective!(ocp, :lagrangian, (x, u) -> 0.5*u^2) # default is to minimise
 
 
 
+
+# Module
+
+function solve(ocp,N)
+
 # transcription ocp -> NLP
-N = 2 # time steps
 m = ocp.control_dimension
 f = ocp.dynamics
 L = ocp.lagrange 
 
+
+function get_state_at_time_step(xu,i)
+  """
+     return
+     x(t_i)
+  """
+  if i > N
+    error("trying to access at x(t_i) for i > N")
+  end  
+  return xu[1+i*(n+1):(i+1)*(n+1)]
+end
+
+function get_control_at_time_step(xu,i)
+  """
+     return
+     u(t_i)
+  """
+  if i > N-1
+    error("trying to access at (t_i) for i > N-1")
+  end
+  return xu[1+(N+1)*(n+1)+i*m:m+(N+1)*(n+1)+i*m]
+end
+
+function parse_sol(xu,n,m,N)
+  """
+     return
+      X : matrix(N+1,n+1)
+      U : matrix(N,m)
+  """
+    X = zeros(N+1,n+1)
+    U = zeros(N,m)
+    for i in 1:N
+      X[i,:] =  get_state_at_time_step(xu,i-1)
+      U[i,:] = get_control_at_time_step(xu,i-1)
+    end
+    X[N+1,:] = get_state_at_time_step(xu,N)
+  return X, U
+end
 # we pass u[1] because in our case ocp.dynamics and ocp.lagrange are define with a scalar u
 # but her x is a vector
 # question : how determine if u and x are scalar or vector ?
@@ -54,39 +96,11 @@ nc = N*(n+1)+2*n+1               # dimension of the constraints
 
 
                                #
+
+
+
+
 objective(xu) = xu[(N+1)*(n+1)]
-
-function get_state_at_time_step(xu,i)
-  """
-     return
-     x(t_i)
-  """
-  if i > N
-    error("trying to access at x(t_i) for i > N")
-  end  
-  return xu[1+i*(n+1):(i+1)*(n+1)]
-end
-
-function get_control_at_time_step(xu,i)
-  """
-     return
-     u(t_i)
-  """
-  if i > N-1
-    error("trying to access at (t_i) for i > N-1")
-  end
-  return xu[1+(N+1)*(n+1)+i*m:m+(N+1)*(n+1)+i*m]
-end
-
-function parse_sol(xu,t0,tf,N)
-  """
-     return
-      X = matrix(n+1,N+1)
-  """
-
-end
-
-
 
 #function constraint(ocp, xu::Vector{<:Real},N)::Vector{<:Real}
 function constraint(ocp, xu, N)
@@ -113,11 +127,11 @@ function constraint(ocp, xu, N)
     n = ocp.state_dimension
     m = ocp.control_dimension
     nc = N*(n+1)+2*n+1               # dimension of the constraints
-    x0 = ocp.initial_condition
-    xf = ocp.final_condition
-  
-   
-                                                      
+
+    #x0 = ocp.initial_condition
+    #xf = ocp.final_condition                                                   
+    x0 = [-1., 0.]
+    xf = [0., 0.]
 
     h = (tf-t0)/N
     c = zeros(eltype(xu),nc)
@@ -147,5 +161,32 @@ xu0 = zeros((N+1)*(n+1)+N*m)
 #nlp = ADNLPModel(objective, xu0, constraint_Ipopt,lb,ub)
 nlp = ADNLPModel(objective, xu0, xu -> constraint(ocp,xu,N),lb,ub)
 stats = ipopt(nlp)
-print(stats)
+X, U = parse_sol(stats.solution,n,m,N)
+time = collect(t0:(tf-t0)/N:tf)
+sol = (time,X,U,n,m,N)
+return sol
+end
+
+
+
+function plot_sol(sol)
+time, X, U, n, m, N = sol
+px = plot(time, X,layout = (n+1,1))
+plot!(px[1],title="state")
+plot!(px[n+1], xlabel="t")
+for i in 1:n+1
+  plot!(px[i],ylabel = string("x_",i))
+end
+pu = plot(time[1:N],U,lc=:red,layout = (m,1))
+for i in 1:m
+  plot!(pu[i],ylabel = string("u_",i))
+end
+plot!(pu[1],title = "control")
+plot!(pu[m],xlabel = "t")
+plot(px,pu,legend = false)
+end
+
+
+sol = solve(ocp,100)
+plot_sol(sol)
 
