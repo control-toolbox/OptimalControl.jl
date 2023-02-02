34a6c7729b76e568565b2dc8ee06f70857646e03
Caillau Jean-Baptiste <jean-baptiste.caillau@univ-cotedazur.fr>
2023-02-02 16:19:43 +0100

    model.jl fix

--- test/test_basic.jl
+++ test/test_basic.jl
@@ -7,19 +7,22 @@ ocp = Model()
 state!(ocp, 2)
 control!(ocp, 1)
 time!(ocp, [t0, tf])
-constraint!(ocp, :initial, [ -1., 0. ])
-constraint!(ocp, :final,   [  0., 0. ])
+constraint!(ocp, :initial, [ -1.0, 0.0 ])
+constraint!(ocp, :final,   [  0.0, 0.0 ])
 
 #
-A = [ 0. 1.
-      0. 0.]
-B = [ 0.
-      1. ]
+A = [ 0.0 1.0
+      0.0 0.0 ]
+B = [ 0.0
+      1.0 ]
 
 constraint!(ocp, :dynamics, (x, u) -> A*x + B*u)
 objective!(ocp, :lagrangian, (x, u) -> 0.5*u^2) # default is to minimise
 
-ξ, ψ, ϕ = nlp_constraints(ocp)
+(ξf, ξl, ξu), (ψf, ψl, ψu), (ϕf, ϕl, ϕu) = nlp_constraints(ocp)
+val = ϕf(0.0, [ -1.0, 0.0 ], 1.0, [ 0.0, 0.0 ])
+println("ϕf = ", val)
+@test length(val) == 4 
 
 #
 @test ocp.state_dimension == 2
@@ -29,7 +32,6 @@ objective!(ocp, :lagrangian, (x, u) -> 0.5*u^2) # default is to minimise
 @test ocp.final_time == tf
 @test ocp.dynamics([0.; 1.], 1.0) ≈ [1.; 1.] atol=1e-8
 @test ocp.lagrangian([0.; 0.], 1.0) ≈ 0.5 atol=1e-8
-
 
 # from model to problem
 # tbd
