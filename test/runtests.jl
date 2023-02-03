19075dd5e872d31f69ea94f73704aabda389c7de
Olivier Cots <olivier.cots@enseeiht.fr>
2023-02-03 16:19:48 +0100

    foo

--- test/runtests.jl
+++ test/runtests.jl
@@ -24,12 +24,12 @@ const nlp_constraints       = OptimalControl.nlp_constraints
 #
 @testset verbose = true showtiming = true "Optimal control tests" begin
     for name in (
-        #"utils", 
-        #"optimal_control", 
-        #"udss", # unconstrained direct simple shooting
-        #"basic",
-        "goddard_direct",
-        #"goddard",
+        "utils", 
+        "optimal_control", 
+        "udss", # unconstrained direct simple shooting
+        "basic",
+        #"goddard_direct",
+        "goddard",
         )
         @testset "$name" begin
             include("test_$name.jl")
