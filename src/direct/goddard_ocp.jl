2d197925a5906757917eda12b3e9502695f4a4fa
gergaud <joseph.gergaud@toulouse-inp.fr>
2023-02-03 10:40:05 +0100

    goddard ok N=40

--- src/direct/goddard_ocp.jl
+++ src/direct/goddard_ocp.jl
@@ -29,7 +29,7 @@ control!(ocp, 1) # control dim
 
 constraint!(ocp, :initial, x0) # vectorial equality
 constraint!(ocp, :control, u -> u[1], 0., 1.) # constraints can be labeled or not
-constraint!(ocp, :state, (x, u) -> x[1], r0-epsilon, Inf, :state_con1)       # epsilon car sinon adjoint state at 0 false because the state is r0 touch the constraint at 0
+constraint!(ocp, :state, (x, u) -> x[1], r0, Inf, :state_con1)       # epsilon car sinon adjoint state at 0 false because the state is r0 touch the constraint at 0
 constraint!(ocp, :state, (x, u) -> x[2], 0., vmax, :state_con2)
 constraint!(ocp, :state, (x, u) -> x[3], mf, m0, :state_con3)        
 
@@ -54,7 +54,7 @@ end
 
 constraint!(ocp, :dynamics, f) # dynamics can be in place
 
-sol = solve(ocp,100)
+sol = solve(ocp,40)
 
 using JLD2
 @save "./src/direct/sol_goddard_100.jld2" sol
