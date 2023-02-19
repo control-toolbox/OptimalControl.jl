ea52bc4ddb5e6100784ccf74c7473d9867d0d0b2
Olivier Cots <olivier.cots@enseeiht.fr>
2023-02-19 15:44:07 +0100

    rm ControlToolboxTools

--- src/OptimalControl.jl
+++ src/OptimalControl.jl
@@ -8,15 +8,9 @@ using Reexport
 using CTDirect
 using CTDirectShooting
 
-# tools: callbacks, exceptions, functions and more
-@reexport using ControlToolboxTools
-
 # flows
 @reexport using HamiltonianFlows
 import HamiltonianFlows: Flow
-
-# Types
-const MyNumber, MyVector, Time, Times, TimesDisc, States, Adjoints, Controls, State, Adjoint, Dimension = CTBase.types()
 
 # Other declarations
 const __display = CTBase.__display
