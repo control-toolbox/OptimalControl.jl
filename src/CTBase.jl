48f8f5459228540ed63c6ade393dffcbdd2968ee
Olivier Cots <olivier.cots@enseeiht.fr>
2023-03-13 13:09:11 +0100

    v0.4.1

--- src/CTBase.jl
+++
@@ -1,6 +0,0 @@
-import Base: show, Base
-# we get an error when a solution is printed so I add this function
-# which has to be put in the package CTBase and has to be completed
-function Base.show(io::IO, ::MIME"text/plain", sol::OptimalControlSolution)
-    nothing
-end
--- src/OptimalControl.jl
+++ src/OptimalControl.jl
@@ -17,11 +17,6 @@ const __display = CTBase.__display
 # resources
 include("solve.jl")
 
-# ----------------------------------------
-# to remove when put in the right package
-include("CTBase.jl")
-# ----------------------------------------
-
 # export functions only for user
 export solve
 
