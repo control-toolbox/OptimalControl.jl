55adbf5f52b53677bad89a1aee32fe501537ad94
Olivier Cots <olivier.cots@enseeiht.fr>
2023-03-13 12:10:13 +0100

    foo

--- test/test_goddard_direct.jl
+++ test/test_goddard_direct.jl
@@ -5,5 +5,5 @@ ocp = prob.model
 init = [1.01, 0.25, 0.5, 0.4]
 sol = solve(ocp, grid_size=10, print_level=0, init=init)
 
-@test objective(sol) ≈ -1.0 atol=1e-1
+@test sol.objective ≈ -1.0 atol=1e-1
 #@test constraints_violation(sol) < 1e-6
