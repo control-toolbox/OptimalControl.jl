ee6809ccec2dd668645e6fc73d102afda0e363f5
Olivier Cots <olivier.cots@irit.fr>
2024-07-08 22:48:41 +0200

    foo

--- docs/make.jl
+++ docs/make.jl
@@ -5,6 +5,7 @@ using CTBase
 using CTFlows
 using CTDirect
 using CTProblems
+using Plots
 
 # https://stackoverflow.com/questions/70137119/how-to-include-the-docstring-for-a-function-from-another-package-in-my-julia-doc
 DocMeta.setdocmeta!(CTBase, :DocTestSetup, :(using CTBase); recursive = true)
@@ -13,11 +14,15 @@ DocMeta.setdocmeta!(CTDirect, :DocTestSetup, :(using CTDirect); recursive = true
 DocMeta.setdocmeta!(CTProblems, :DocTestSetup, :(using CTProblems); recursive = true)
 DocMeta.setdocmeta!(OptimalControl, :DocTestSetup, :(using OptimalControl); recursive = true)
 
+Base.showable(::MIME"text/html", ::Plots.Plot) = false
+
 makedocs(
     warnonly = :cross_references,
     sitename = "OptimalControl.jl",
     format = Documenter.HTML(prettyurls = false,
-                             size_threshold_ignore = ["api-ctbase.md"]),
+                             size_threshold_ignore = [  "api-ctbase.md", 
+                                                        "application-mri-saturation.md"
+                                                     ]),
     pages = [
         "Introduction" => "index.md",
         "Applications" => [
