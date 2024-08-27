7d655757eaf07307ab024300c333ec5a3d6cc708
Olivier Cots <olivier.cots@irit.fr>
2024-08-27 10:42:58 +0200

    obj

--- test/test_goddard_direct.jl
+++ test/test_goddard_direct.jl
@@ -1,10 +1,10 @@
 function test_goddard_direct()
 
     # goddard with state constraint - maximize altitude
-    ocp, objective = Goddard()
+    ocp, obj = Goddard()
 
     # initial guess (constant state and control functions)
     init = (state = [1.01, 0.05, 0.8], control = 0.1, variable = 0.2)
     sol = solve(ocp; grid_size = 10, display = false, init = init)
-    @test objective(sol) ≈ objective atol = 5e-3
+    @test objective(sol) ≈ obj atol = 5e-3
 end
