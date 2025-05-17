ace6e59b3a20bb04155d073bb4f7973c1b28eb2f
Olivier Cots <olivier.cots@irit.fr>
2025-05-17 13:19:21 +0200

    add extra flow

---
+++ test/extras/nonautonomous.jl
@@ -0,0 +1,30 @@
+using OptimalControl
+using NLPModelsIpopt
+using Plots
+
+t0 = 0
+tf = π/4
+x0 = 0
+xf = tan(π/4) - 2log(√(2)/2)
+
+ocp = @def begin
+
+    t ∈ [t0, tf], time
+    x ∈ R, state
+    u ∈ R, control
+
+    x(t0) == x0
+    x(tf) == xf
+
+    ẋ(t) == u(t) * (1 + tan(t))
+
+    0.5∫( u(t)^2 ) → min
+
+end;
+
+sol = solve(ocp; print_level=4);
+
+plt = plot(sol, ocp)
+
+u(t) = 1 + tan(t)
+plot!(plt[3], range(t0, tf, 101), u; color=:red, legend=false)
