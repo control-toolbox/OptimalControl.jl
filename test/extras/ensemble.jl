7c262674066d76f735ae82a779626815176acc03
Olivier Cots <olivier.cots@irit.fr>
2025-06-13 14:11:20 +0200

    foo

---
+++ test/extras/ensemble.jl
@@ -0,0 +1,21 @@
+using OptimalControl
+using NLPModelsIpopt
+using Plots
+
+c0(N) = repeat([-1, 0], N)
+cf(N) = repeat([ 0, 0], N)
+f(x, u) = [x[2], u]
+f(x, u, N) = reduce(vcat, [f(x[2i-1:2i], u) for i ∈ 1:N])
+
+N = 3
+ocp = @def begin
+    t ∈ [0, 1], time
+    x ∈ R^(2N), state
+    u ∈ R, control
+    x(0) == c0(N)
+    x(1) == cf(N)
+    ẋ(t) == f(x(t), u(t), N)
+    ∫( 0.5u(t)^2 ) → min
+end
+
+sol = solve(ocp)
