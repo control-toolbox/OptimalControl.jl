3b42608b8a9b536a954dcca1f3829bb26c78f7bb
Olivier Cots <olivier.cots@irit.fr>
2025-07-04 21:35:11 +0200

    add compat

--- test/runtests.jl
+++ test/runtests.jl
@@ -16,17 +16,17 @@ using SplitApplyCombine # for flatten in some tests
     # ctdirect tests
     @testset verbose = true showtiming = true "CTDirect tests" begin
         # run all scripts in subfolder suite/
-        include.(filter(contains(r"test_exa.jl$"), readdir("./ctdirect/suite"; join=true)))
+        include.(filter(contains(r".jl$"), readdir("./ctdirect/suite"; join=true)))
     end
 
-    # # other tests: indirect
-    # include("./indirect/Goddard.jl")
-    # for name in (:goddard_indirect,)
-    #     @testset "$(name)" begin
-    #         test_name = Symbol(:test_, name)
-    #         println("Testing: " * string(name))
-    #         include("./indirect/$(test_name).jl")
-    #         @eval $test_name()
-    #     end
-    # end
+    # other tests: indirect
+    include("./indirect/Goddard.jl")
+    for name in (:goddard_indirect,)
+        @testset "$(name)" begin
+            test_name = Symbol(:test_, name)
+            println("Testing: " * string(name))
+            include("./indirect/$(test_name).jl")
+            @eval $test_name()
+        end
+    end
 end
