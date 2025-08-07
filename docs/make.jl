436ad096c2e95f794d057de166327a9b0f7f4e35
Jean-Baptiste Caillau <jean-baptiste.caillau@univ-cotedazur.fr>
2025-08-07 18:51:00 +0200

    added manual-ai-ded.md

--- docs/make.jl
+++ docs/make.jl
@@ -120,7 +120,7 @@ cp(
 repo_url = "github.com/control-toolbox/OptimalControl.jl"
 
 makedocs(;
-    draft=false, # if draft is true, then the julia code from .md is not executed # debug
+    draft=true, # if draft is true, then the julia code from .md is not executed # debug
     # to disable the draft mode in a specific markdown file, use the following:
     # ```@meta
     # Draft = false
@@ -144,6 +144,7 @@ makedocs(;
         ],
         "Manual" => [
             "Define a problem" => "manual-abstract.md",
+            "Use AI" => "manual-ai-ded.md",
             "Problem characteristics" => "manual-model.md",
             "Set an initial guess" => "manual-initial-guess.md",
             "Solve a problem" => "manual-solve.md",
